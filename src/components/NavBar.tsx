9543fad0888e76bcccb064f562b1328154ca4892
EtiennePONS <etienne.pons@hotmail.fr>
2023-01-30 14:03:05 +0100

    Design NavBar fini

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,36 +4,72 @@ import "./NavBar.css";
 const NavBar = () => {
   return (
     <div>
-      <nav className="navbar shadow" data-bs-theme="">
+      <nav className="navbar">
         <form className="container-fluid justify-content-start">
           <NavLink to="/" end className="nav-link">
             <button className="btn btn-lg btn-bd-primary me-4" type="button">
-              Page-Home
+              <img
+                src="/icons/PageHome.png"
+                alt="Logo Frigo"
+                width="80"
+                height="80"
+                className="LogoNavbar1"
+              />
             </button>
           </NavLink>
           <NavLink to="/pagePlay" end className="nav-link">
             <button className="btn btn-lg btn-bd-primary me-4" type="button">
-              Page-Play
+              <img
+                src="/icons/PagePlay.png"
+                alt="Logo Frigo"
+                width="80"
+                height="80"
+                className="LogoNavbar2"
+              />
             </button>
           </NavLink>
           <NavLink to="/pageInscription" end className="nav-link">
             <button className="btn btn-lg btn-bd-primary me-4" type="button">
-              Inscription
+              <img
+                src="/icons/PageInscription.png"
+                alt="Logo Frigo"
+                width="80"
+                height="80"
+                className="LogoNavbar3"
+              />
             </button>
           </NavLink>
           <NavLink to="/pageConnexion" end className="nav-link">
             <button className="btn btn-lg btn-bd-primary me-4" type="button">
-              Connexion
+              <img
+                src="/icons/PageConnexion.png"
+                alt="Logo Frigo"
+                width="80"
+                height="80"
+                className="LogoNavbar4"
+              />
             </button>
           </NavLink>
           <NavLink to="/pageMédiathèque" end className="nav-link">
             <button className="btn btn-lg btn-bd-primary me-4" type="button">
-              Médiathèque
+              <img
+                src="/icons/PageMedia.png"
+                alt="Logo Frigo"
+                width="80"
+                height="80"
+                className="LogoNavbar5"
+              />
             </button>
           </NavLink>
           <NavLink to="/pageGalerie" end className="nav-link">
             <button className="btn btn-lg btn-bd-primary me-4" type="button">
-              Galerie
+              <img
+                src="/icons/PageGalerie.png"
+                alt="Logo Frigo"
+                width="80"
+                height="80"
+                className="LogoNavbar6"
+              />
             </button>
           </NavLink>
         </form>
